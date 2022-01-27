dfed94a430a6c69504eebd3a9d2d9d0fedacb90e
Joel Quintyn <joel.quintyn@devstation-t430.com>
2022-01-27 11:51:34 -0400

    Updated user admin view

--- users/admin.py
+++ users/admin.py
@@ -1,15 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """
     Custom User Admin Display/View
     """
 
-    list_display = ("username", "email", "gender", "language", "currency", "superhost")
-    list_filter = ("superhost", "language", "currency")
+    # list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    # list_filter = ("superhost", "language", "currency")
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "bithdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
