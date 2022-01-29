a4b72219e45473b6fedd2ec5e51537a3acd86c6a
Joel Quintyn <joel.quintyn@devstation-t430.com>
2022-01-29 14:15:32 -0400

    Updated 'rooms' app.

--- rooms/models.py
+++ rooms/models.py
@@ -18,13 +18,68 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
-    pass
+    """
+    RoomType Model Definition
+        to add and remove a room type.
+    """
+
+    class Meta:
+        verbose_name = "Room Type"
+        verbose_name_plural = "Room Types"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+    """
+    Amenity Model Definition
+        to add and remove amenities.
+    """
+
+    class Meta:
+        verbose_name = "Amenity"
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    """
+    Facility Model Definition
+        to add and remove facilities.
+    """
+
+    class Meta:
+        verbose_name = "Facility"
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    """
+    HouseRule Model Definition
+        to add and remove house rules.
+    """
+
+    class Meta:
+        verbose_name = "House Rule"
+        verbose_name_plural = "House Rules"
+
+
+class Photo(core_models.TimeStampedModel):
+    """
+    Photo Model Definition
+        to add photos for a room
+    """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
 
     """
-    Room Model definition
+    Room Model Definition
     """
 
     name = models.CharField(max_length=140)
@@ -40,8 +95,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
