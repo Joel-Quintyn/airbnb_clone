e1e97d6314ec7fe657630593d838258151aabd23
Joel Quintyn <joel.quintyn@devstation-t430.com>
2022-01-30 02:10:48 -0400

    Updated 'RoomAdmin' display.

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,78 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Information",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "room_type",
+                    "address",
+                    "country",
+                    "city",
+                    "price",
+                )
+            },
+        ),
+        (
+            "Booking",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "More about the space",
+            {"fields": ("guests", "beds", "bedrooms", "baths")},
+        ),
+        (
+            "What this place offers",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        (
+            "Last Details",
+            {"fields": ("host", "created", "updated")},
+        ),
+    )
+
+    list_display = (
+        "name",
+        "host",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "room_type",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        # "city",
+        "country",
+    )
+
+    search_fields = (
+        "country",
+        "city",
+        "^host__username",
+    )
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    readonly_fields = ("created", "updated")
 
 
 @admin.register(models.Photo)
