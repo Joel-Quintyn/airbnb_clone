a178c7eafb0d15ad41a733e0f99c50522a76b72f
Joel Quintyn <joel.quintyn@devstation-t430.com>
2022-02-12 12:59:47 -0400

    Updated views.py for rooms

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,37 @@
-from pyexpat import model
-from django.shortcuts import render
+# from math import ceil
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/all_rooms.html", context={"rooms": all_rooms})
+    page = request.GET.get("page", 1)
+    rooms_list = models.Room.objects.all()
+    paginator = Paginator(rooms_list, 10)
+    # print(vars(rooms.paginator))
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/all_rooms.html", context={"page": rooms})
+    except EmptyPage:
+        return redirect("/")
+
+
+#  Manual Pagination Method
+# def all_rooms(request):
+#     page = int(request.GET.get("page", 1))
+#     page_size = 10
+#     limit = page_size * page
+#     offset = limit - page_size
+#     all_rooms = models.Room.objects.all()[offset:limit]
+#     page_count = ceil(models.Room.objects.count() / page_size)
+#     page_range = range(1, page_count + 1)
+#     return render(
+#         request,
+#         "rooms/all_rooms.html",
+#         context={
+#             "rooms": all_rooms,
+#             "page": page,
+#             "page_count": page_count,
+#             "page_range": page_range,
+#         },
+#     )
