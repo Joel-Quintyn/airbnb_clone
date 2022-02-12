f06874eca6b00c31a0d2191033dabb2f8a190d23
Joel Quintyn <joel.quintyn@devstation-t430.com>
2022-02-12 00:10:35 -0400

    Created templating for project

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
+from pyexpat import model
 from django.shortcuts import render
+from . import models
 
 
-def all_rooms():
-    pass
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/all_rooms.html", context={"rooms": all_rooms})
