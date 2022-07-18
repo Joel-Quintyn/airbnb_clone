efd88360ef665a16fa92d3a856952133e9e9a198
Joel Quintyn <joel.quintyn@devstation-t430.com>
2022-07-18 16:55:53 -0400

    Changed room detail view to a CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,4 @@
-from django.views.generic import ListView
-from django.http import Http404
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -13,9 +11,15 @@ class HomeView(ListView):
     ordering = "created"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+
+    model = models.Room
+
+
+""" Function based view for the room_detail view """
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
