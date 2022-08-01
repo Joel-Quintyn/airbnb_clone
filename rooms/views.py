3b11f478f5b16ecbbc761f076b62a5f8660844f6
Joel Quintyn <joel.quintyn@devstation-t430.com>
2022-08-01 14:12:31 -0400

    Updated search view/form.

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,5 @@
+import re
+from statistics import mode
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from django_countries import countries
@@ -18,17 +20,45 @@ class RoomDetail(DetailView):
     model = models.Room
 
 
-""" Function based view for the room_detail view """
-# def room_detail(request, pk):
-#     try:
-#         room = models.Room.objects.get(pk=pk)
-#         return render(request, "rooms/detail.html", {"room": room})
-#     except models.Room.DoesNotExist:
-#         raise Http404()
-
-
 def search_view(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
+    country = request.GET.get("country", "AF")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    beds = int(request.GET.get("beds", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant_book = request.GET.get("instant_book", False)
+    superhost = request.GET.get("superhost", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "beds": beds,
+        "bedrooms": bedrooms,
+        "baths": baths,
+        "instant_book": instant_book,
+        "superhost": superhost,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+    }
+
     room_types = models.RoomType.objects.all()
-    return render(request, "rooms/search.html", {"city": city, "countries": countries, "room_types": room_types})
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices})
