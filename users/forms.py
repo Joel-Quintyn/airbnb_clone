d3390e76b2067c7a0798d9c23fd4ffdf17a701a3
Joel Quintyn <joel.quintyn@devstation-t430.com>
2022-08-03 00:53:46 -0400

    Updated validation check for user email & password

--- users/forms.py
+++ users/forms.py
@@ -7,22 +7,14 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(username=email)
-            return email
-        except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exsist")
-
-    def clean_password(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
         try:
-            user = models.User.objects.get(username=email)
+            user = models.User.objects.get(email=email)
             if user.check_password(password):
-                return password
+                return self.cleaned_data
             else:
-                raise forms.ValidationError("Check password")
+                self.add_error("password", forms.ValidationError("Check password"))
         except models.User.DoesNotExist:
-            pass
+            self.add_error(forms.ValidationError("User does not exist"))
